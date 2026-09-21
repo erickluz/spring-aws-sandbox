24fb5920478442af0ee1359326c99c2543d75884
Erick Luz <erickluz360@gmail.com>
2026-09-21 17:43:15 -0300

    Refactor SQSIdempotenciaTest to enhance idempotency checks and message processing

--- src/test/java/luz/erick/spring_aws_sandbox/repository/SQSIdempotenciaTest.java
+++ src/test/java/luz/erick/spring_aws_sandbox/repository/SQSIdempotenciaTest.java
@@ -1,9 +1,9 @@
 package luz.erick.spring_aws_sandbox.repository;
 
-import java.security.InvalidKeyException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import lombok.extern.slf4j.Slf4j;
 import luz.erick.spring_aws_sandbox.config.SqsConfig;
 import software.amazon.awssdk.services.sqs.model.Message;
+import tools.jackson.databind.ObjectMapper;
 
 @SpringBootTest 
 @Slf4j 
@@ -23,42 +24,67 @@ public class SQSIdempotenciaTest {
     @Autowired 
     private SqsConfig sqsConfig;
 
+    @Autowired 
+    private ObjectMapper objectMapper;
+
     List<String> messagesId;
 
     @Test 
     void deveConsumirMensagemComIdempotencia() {
-
+        String URLBase = sqsConfig.getUrlBase();
         messagesId = new ArrayList<String>();
-
         sqsRepository.createQueue("q1");
 
-        String msg1 = "msg1";
-        String msg2 = "msg2";
+        String msg1 = """
+            {
+                "eventId": 1, 
+                "cliente":
+                    {
+                        "nome": "Erick",
+                        "idade": 20
+                    }
+            }""";
         sqsRepository.sendMessage("q1", msg1);
-        sqsRepository.sendMessage("q1", msg2);
 
-        String URLBase = sqsConfig.getUrlBase();
         List<Message> receivedMessages = sqsRepository.receiveMessages(URLBase + "q1");
-        List<Message> mensagens = processarMensagens(receivedMessages, URLBase);
+        List<Cliente> clientes = processarMensagens(receivedMessages, URLBase);
 
-        // Deve verificar idempotencia
         Assertions.assertThrows(IllegalArgumentException.class, () -> processarMensagens(receivedMessages, URLBase));
         
-        log.info("Mensagens: {}", mensagens);
+        log.info("Clientes: {}", clientes);
     }
 
-    private List<Message> processarMensagens(List<Message> messages, String URLBase) {
-        List<Message> mensagens = new ArrayList<Message>();
-        log.info("recevedMessages: {}", messages);
+    private List<Cliente> processarMensagens(List<Message> messages, String URLBase) {
+        List<Cliente> mensagens = new ArrayList<Cliente>();
+        log.info("receivedMessages: {}", messages);
         messages.forEach(m -> {
-            if (messagesId.contains(m.messageId())) {
+            Evento evento = parseEvento(m);
+            if (messagesId.contains(evento.eventId())) {
+                sqsRepository.deleteMessage(URLBase + "q1", Lists.list(m));
                 throw new IllegalArgumentException();
             }
-            messagesId.add(m.messageId());
-            mensagens.add(m);
+            messagesId.add(evento.eventId());
+            mensagens.add(evento.cliente());
             sqsRepository.deleteMessage(URLBase + "q1", messages);
         });
         log.info("Messagesid: {}", messagesId);
         return mensagens;
     }
+
+    private Evento parseEvento(Message m) {
+        Evento evento;
+        try {
+            evento = toEvento(m.body());
+        } catch (Exception e) {
+            log.error("Erro ao processar evento", e);
+            throw new IllegalArgumentException();
+        }
+        return evento;
+    }
+
+    public Evento toEvento(String json) throws Exception {
+        return objectMapper.readValue(json, Evento.class);
+    }
 }
+record Evento (String eventId, Cliente cliente) {};
+record Cliente (String nome, Integer idade) {}
